9231428c74c8bf3ac7dbd4bcfbdfee3327d0f6dc
Aditya Balasubramanyam <35840952+aditya8119@users.noreply.github.com>
2020-08-30 18:05:51 +0530

    Update Controller.js
    Update IP address of AWS Deployment Server

--- src/screens/Controller.js
+++ src/screens/Controller.js
@@ -10,7 +10,7 @@ class Controller extends Component {
   constructor()
   {
     super();
-    this.baseUrl = "http://18.212.74.66:8080/api/v1/";
+    this.baseUrl = "http://18.234.128.111:8080/api/v1/";
   }
   render(){
     return(
